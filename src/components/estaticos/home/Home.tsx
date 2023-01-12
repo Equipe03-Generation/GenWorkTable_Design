8cae8eed95205eb45aa12af851d53a630cd4b703
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2023-01-11 21:20:54 -0300

    Add files via upload

---
+++ src/components/estaticos/home/Home.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
+import './Home.css';
+import { Link } from 'react-router-dom'
+import ModalProjeto from '../../modais/modalProjeto/ModalProjeto';
+import ListaProjetos from '../../paginas/listaprojetos/ListaProjetos';
+
+function Home() {
+    
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem-vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Blog para amantes de programação!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                            <ModalProjeto />
+                        </Box>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Button variant="outlined" className='botao'>Ver Projetos</Button>
+                        </Link>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://cdn-icons-png.flaticon.com/512/1442/1442569.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} className='Projetos'>
+                    <ListaProjetos />
+                </Grid>
+            </Grid>
+}
+
+export default Home;
