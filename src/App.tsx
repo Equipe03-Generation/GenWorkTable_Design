c449e361154c1747ab5c2e09e6be24279a87a979
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2023-01-11 21:12:37 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ListaProjetos from './components/paginas/listaprojetos/ListaProjetos';
+import ListaTurmas from './components/paginas/listaturmas/ListaTurmas';
+import ListaGrupos from './components/paginas/listagrupos/ListaGrupos';
+import CustomNavbar from './components/estaticos/navbar/GenWorks';
+import CadastroTurma from './components/cadastros/CadastroTurma/CadastroTurma';
+import CadastroProjeto from './components/cadastros/cadastroProjeto/CadastroProjeto';
+import CadastroGrupo from './components/cadastros/cadastroGrupo/CadastroGrupo';
+import Home from './components/estaticos/home/Home';
+import Footer from './components/estaticos/footer/Footer';
+
+function App() {
+  return (
+    <Router>
+      <CustomNavbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/grupos' element={<ListaGrupos />} />
+        <Route path='/projetos' element={<ListaProjetos />} />
+        <Route path='/turmas' element={<ListaTurmas />} />
+        <Route path='/cadastrogrupo' element={<CadastroGrupo />} />
+        <Route path='/cadastroprojeto' element={<CadastroProjeto />} />
+        <Route path='/cadastroturma' element={<CadastroTurma />} />
+      </Routes>
+      <Footer />
+    </Router>
+
+  );
+}
+
+export default App;
